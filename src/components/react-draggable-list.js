13c56ae4b26c0d4124c1f75cbc6aea458b476ec2
afeiship <1290657123@qq.com>
2018-05-23 12:18:21 +0800

    issue: pure -> component

--- src/components/react-draggable-list.js
+++ src/components/react-draggable-list.js
@@ -1,4 +1,4 @@
-import React,{PureComponent} from 'react';
+import React,{ Component } from 'react';
 
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -6,7 +6,7 @@ import noop from 'noop';
 import objectAssign from 'object-assign';
 import Sortable from 'sortablejs';
 
-export default class extends PureComponent{
+export default class extends Component{
   /*===properties start===*/
   static propTypes = {
     className: PropTypes.string,
